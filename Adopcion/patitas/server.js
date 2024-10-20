cf923e39a03a75a8a55af031bc5f05734ba541ee
Ninii94 <nicki.pastrana@gmail.com>
2024-10-19 23:38:01 -0300

    modificacion 1ra

--- Adopcion/patitas/server.js
+++ Adopcion/patitas/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, 'adopcion/build')));
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
